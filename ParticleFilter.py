5600af6e74157584566bf27d46967709ad751b01
Abdurrahman Abuzaid <abdurrahman.abzd@gmail.com>
2023-11-22 20:52:54 -0500

    Add steps comments

--- ParticleFilter.py
+++ ParticleFilter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Import Map file and save it in an array
 file_path = 'map2023.dat'
 with open(file_path, 'r') as file:
     map_data = file.read()
@@ -12,3 +13,24 @@ mapInverted = 1 - mapArray
 
 plt.imshow(mapInverted, cmap='gray')
 plt.show()
+
+# Initialize Particles
+# - Particle Number
+# - Initial Particles Position
+
+# Import Robot Log
+# if "L": 
+# 1- Motion Model (odometry data)***
+
+# 2- Sensor Model (laser data)***
+
+# 3- Resampling (Low  Variance Algorithm) ***
+
+# if "O":
+# 1- Motion Model
+  
+
+# Plotting
+
+# Video Creation
+

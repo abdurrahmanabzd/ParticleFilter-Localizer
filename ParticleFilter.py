4420c81acf555911a2707ec30368270ea15d0c3e
Abdurrahman Abuzaid <abdurrahman.abzd@gmail.com>
2023-12-04 21:35:42 -0500

    Fix functions

--- ParticleFilter.py
+++ ParticleFilter.py
@@ -11,8 +11,8 @@ num_particles = 1000
 initial_particles = np.random.rand(num_particles, 4)  # Each particle is [x, y, theta, weight]
 
 # Initialize particles within a specific region
-initial_particles[:, 0] = np.random.uniform(72, 85, num_particles)  # x position
-initial_particles[:, 1] = np.random.uniform(445, 465, num_particles)  # y position
+initial_particles[:, 0] = np.random.uniform(445, 465, num_particles)  # x position
+initial_particles[:, 1] = np.random.uniform(72, 85, num_particles)  # y position
 initial_particles[:, 2] = np.random.uniform(-np.pi, np.pi, num_particles)  # theta
 initial_particles[:, 3] = 1 #initial weight = 1
 
@@ -39,122 +39,144 @@ def show_particles(mapArray,particles):
     plt.scatter(particles[:, 0], particles[:, 1], s=1, c='b', marker='.')
     plt.show()
 
-# Particle filter odometry motion update function
 def motion_model(particles, delta_rot1, delta_trans, delta_rot2):
     """
-    Update particle positions based on odometry motion model.
+    Update particle positions based on motion model.
 
     Parameters:
     - particles: numpy array of shape (num_particles, 4) representing particles [x, y, theta, weight]
-    - delta_rot1: array of delta rotation1 values for each particle
-    - delta_trans: array of delta translation values for each particle
-    - delta_rot2: array of delta rotation2 values for each particle
-    
+    - delta_rot1: change in orientation before motion (rad)
+    - delta_trans: translational change (cm)
+    - delta_rot2: change in orientation after motion (rad)
+
     Returns:
-    - Updated particles array after odometry motion update.
+    - Updated particles array with adjusted positions based on motion model.
     """
-    particles[:, 0] += delta_trans * np.cos(particles[:, 2] + delta_rot1)
-    particles[:, 1] += delta_trans * np.sin(particles[:, 2] + delta_rot1)
-    particles[:, 2] += delta_rot1 + delta_rot2
+    alpha_1 = 1e-6
+    alpha_2 = 1e-6
+    alpha_3 = 1e-6
+    alpha_4 = 1e-6
+
+    # Add noise to motion model
+    for i in range(len(particles)):
+        # Extract particle's pose
+        x, y, theta = particles[i, 0], particles[i, 1], particles[i, 2]
+
+        # Add noise to each motion parameter
+        delta_rot1_hat = delta_rot1 - np.random.normal(0, np.sqrt(alpha_1 * abs(delta_rot1) + alpha_2 * delta_trans))
+        delta_trans_hat = delta_trans - np.random.normal(0, np.sqrt(alpha_3 * delta_trans + alpha_4 * (abs(delta_rot1) + abs(delta_rot2))))
+        delta_rot2_hat = delta_rot2 - np.random.normal(0, np.sqrt(alpha_1 * abs(delta_rot2) + alpha_2 * delta_trans))
+
+        # Update particle pose
+        x_new = x + delta_trans_hat * np.cos(theta + delta_rot1_hat)
+        y_new = y + delta_trans_hat * np.sin(theta + delta_rot1_hat)
+        theta_new = (theta + delta_rot1_hat + delta_rot2_hat) % (2 * np.pi)
+
+        # Update particle position in the array
+        particles[i, 0] = x_new
+        particles[i, 1] = y_new
+        particles[i, 2] = theta_new
 
     return particles
 
-def sensor_model(particles, lidar_data, map):
+
+def sensor_model(particles, sensor_data, grid_map):
     """
-    Update particles' weight based on lidar scan data.
+    Update particle weights based on sensor (laser) data.
 
     Parameters:
     - particles: numpy array of shape (num_particles, 4) representing particles [x, y, theta, weight]
-    - lidar_data: lidar sensor scan data (180 beams)
-    - map: grid map array
+    - sensor_data: list of 180 laser range scanner measurements (180 elements)
+    - grid_map: 2D numpy array representing the occupancy grid map
 
     Returns:
-    - Updated particles array after odometry motion update.
+    - Updated particles array with modified weights based on sensor data likelihood.
     """
-    z_max = 8000  #Maximum sensor range in cm (Zmax threshold)
-    grid_resolution = 4 
+    sigma = 5  # Laser measurement error (cm)
+    z_max = 8000  # Maximum sensor range
 
-    # Sensor model parameters:
-    alpha_1 = 1e-6  #(rad^2/rad^2)
-    alpha_2 = 1e-6  #(rad^2/cm^2)
-    alpha_3 = 1e-6  #(cm^2/cm^2)
-    alpha_4 = 1e-6  #(cm^2/rad^2)
-    sigma = 5  #(cm)
-    lmbda = 0.1  #(cm^-1)
+    # Sensor model parameters
     z_short = 0.09
     z_hit = 0.9
     z_max_prob = 0.01
     z_rand = 0.001
 
-    for particle in particles:
-        x_robot, y_robot, theta_robot, weight = particle
+    # Iterate through each particle
+    for i in range(len(particles)):
+        particle_x, particle_y, particle_theta = particles[i, 0], particles[i, 1], particles[i, 2]
 
-        # Extracting relevant data from the lidar scan
-        x_laser = x_robot + 25 * np.cos(theta_robot)  # x_laser in global coordinates
-        y_laser = y_robot + 25 * np.sin(theta_robot)  # y_laser in global coordinates
+        # Predicted laser scan from the particle's position
+        predicted_scan = []
+        for angle in range(-89, 91):  # Angles from -89 to 90 degrees
+            angle_rad = np.radians(angle) + particle_theta
+            x_end = int(particle_x + z_max * np.cos(angle_rad))
+            y_end = int(particle_y + z_max * np.sin(angle_rad))
 
-        laser_angles = np.deg2rad(np.linspace(theta_robot - 89.5, theta_robot + 89.5, 180)) 
-        ranges = np.array(lidar_data[0:180])  # Laser range measurements
+            # Ray casting to simulate laser measurement
+            hit = False
+            for r in range(1, z_max + 1):
+                x_check = int(particle_x + r * np.cos(angle_rad))
+                y_check = int(particle_y + r * np.sin(angle_rad))
 
-        x_endpoints = x_laser + ranges * np.cos(laser_angles) 
-        y_endpoints = y_laser + ranges * np.sin(laser_angles) 
+                if x_check == x_end and y_check == y_end:
+                    # Reached the maximum sensor range
+                    break
+                if x_check < 0 or y_check < 0 or x_check >= grid_map.shape[0] or y_check >= grid_map.shape[1]:
+                    # Ray went outside the map
+                    break
+                elif grid_map[x_check, y_check] == 1:  # Hit an occupied cell
+                    hit = True
+                    predicted_scan.append(r)
+                    break
 
-        updated_weight = 1.0  #Default weight
+            if not hit:
+                predicted_scan.append(z_max)
 
-        for i in range(len(ranges)):
-            if ranges[i] < z_max:
-                # Convert endpoints to grid cell coordinates
-                x_endpoint_cell = int(np.round(x_endpoints[i] / grid_resolution))
-                y_endpoint_cell = int(np.round(y_endpoints[i] / grid_resolution))
+        # Calculate likelihood based on predicted and actual sensor data
+        likelihood = 1.0
+        for j in range(len(sensor_data)):
+            if sensor_data[j] < z_max:
+                p_hit = np.exp(-0.5 * ((sensor_data[j] - predicted_scan[j]) / sigma) ** 2) / (sigma * np.sqrt(2 * np.pi))
+                # Set p_hit to zero if the ray goes outside the map
+                if predicted_scan[j] == z_max:
+                    p_hit = 0
+                likelihood *= z_hit * p_hit + z_rand * (1.0 / z_max)
+            else:
+                if sensor_data[j] == z_max:
+                    likelihood *= z_max_prob
+                else:
+                    likelihood *= z_short
 
-                # Update probabilities within the map boundaries
-                if 0 <= x_endpoint_cell < map.shape[0] and 0 <= y_endpoint_cell < map.shape[1]:
-                    x_diff = x_endpoint_cell - x_robot / grid_resolution
-                    y_diff = y_endpoint_cell - y_robot / grid_resolution
-                    distance = np.sqrt(x_diff ** 2 + y_diff ** 2)
+        # Update particle weight
+        particles[i, 3] *= likelihood
 
-                    # Sensor model calculations
-                    if ranges[i] == z_max:
-                        p_hit = z_max_prob
-                    else:
-                        p_hit = z_hit * np.exp(-0.5 * (distance ** 2) / (sigma ** 2)) / (sigma * np.sqrt(2 * np.pi))
-
-                    p_short = z_short * (1 / (1 - np.exp(-lmbda * ranges[i]))) if ranges[i] < z_max else 0
-                    p_rand = z_rand / z_max
-
-                    prob = (p_hit + p_short + p_rand) / (p_hit + p_short + p_rand + z_max_prob)
-
-                    # Update particle's weight based on sensor model
-                    updated_weight *= prob
-
-        # Update particle's weight in the particles array
-        particle[3] = updated_weight
+    # Normalize weights
+    particles[:, 3] /= np.sum(particles[:, 3])
 
     return particles
 
+
 def resample(particles):
-    """
-    Resample particles based on their weights.
-    
-    Parameters:
-    - particles: numpy array of shape (num_particles, 4) representing particles [x, y, theta, weight]
+    num_particles = len(particles)
+    weights = particles[:, 3]  # Extract weights from particles
 
-    Returns:
-    - Resampled particles array.
-    """
-    # Re-normalizing the widths 
-    norm_weights = particles[:,3] / np.sum(particles[:,3])
+    # Resampling wheel
+    indices = np.zeros(num_particles, dtype=int)
+    cumulative_sum = np.cumsum(weights)
+    step_size = cumulative_sum[-1] / num_particles
+    beta = 0.0
 
-    # Random Sampling (uniform dist.) 
-    r_s = np.random.choice(np.arange(num_particles), size=num_particles, p=norm_weights)
+    for i in range(num_particles):
+        beta += step_size
+        while beta > cumulative_sum[indices[i]]:
+            indices[i] += 1
 
-    # Creating a new set of particles
-    resampled_particles = particles[r_s].copy()
-
-    # Resetting weights after resampling
-    resampled_particles[:,3] = 1.0 / num_particles
+    # Resample particles based on selected indices
+    resampled_particles = particles[indices]
+    resampled_particles[:, 3] = 1.0 / num_particles  # Reset weights to 1
 
     return resampled_particles
+
 
 #endregion
 
